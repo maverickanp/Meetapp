603314512fc1db42d624c35d150359743d7f7638
Artur Pedrosa <maverickanp@gmail.com>
2019-09-24 18:54:56 -0300

    configuration on sequelize and archtectural folders

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: '0.0.0.0',
+  username: 'root',
+  password: 'docker',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
