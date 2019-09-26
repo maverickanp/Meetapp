771922e0debf6c17fdd1ee547602c30f8ff534f6
Artur Pedrosa <maverickanp@gmail.com>
2019-09-26 12:32:59 -0300

    added validation to avoid Users having duplicated emails

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,18 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
-    return res.json(user);
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
