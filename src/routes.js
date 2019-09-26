3b7a88d8ae601da2e155ad404cafc011010a5713
Artur Pedrosa <maverickanp@gmail.com>
2019-09-26 14:21:16 -0300

    added middleware for authorization

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/sessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
+
+// middleware global
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
